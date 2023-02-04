b4c415229169d9452717a1143cd3d68bfb199aa8
pieris <pieris@tuta.io>
2023-02-04 12:15:09 +0100

    improve getPortfolio() a little

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,20 +1,16 @@
 import getPortfolio from './query.js'
 
-async function displayPortfolio () {
-  const data = await getPortfolio()
+async function displayPortfolio (callback) {
+  const data = callback()
 
   for (let index = 0; index < data.length; index++) {
-    const dataSet = new Set()
-    dataSet.add(data[index].categoryId) // search for categoryId
-    console.log(dataSet)
-
     const fig = document.createElement('figure')
     const image = document.createElement('img')
     const caption = document.createElement('figcaption')
 
     image.src = data[index].imageUrl
     image.alt = data[index].title
-    caption.innerHTML = data[index].title.toUpperCase() // find for another way to print text
+    caption.innerHTML = data[index].title.toUpperCase() // find for another way to print the text
 
     document.getElementsByClassName('gallery')[0].appendChild(fig)
     fig.appendChild(image)
@@ -22,4 +18,9 @@ async function displayPortfolio () {
   }
 }
 
-displayPortfolio().catch(e => console.log(e))
+displayPortfolio(async () => await getPortfolio()).catch(e => console.log(e))
+
+// here is trash code ;þ
+//    const dataSet = new Set()
+//    dataSet.add(data[index].categoryId) // search for categoryId
+//    console.log(dataSet)
