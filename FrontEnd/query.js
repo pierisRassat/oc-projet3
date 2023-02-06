523b02c5e2d19369ab01087114c4ea901752518b
framend <pieris@tuta.io>
2023-02-06 17:07:49 +0100

    improve error handling a little

--- FrontEnd/query.js
+++ FrontEnd/query.js
@@ -1,8 +1,6 @@
-const portfolioApiUrl = 'http://localhost:5678/api/works'
+import { handleError } from './error-handler.js'
 
-const handleError = error => {
-  console.error(error)
-}
+const portfolioApiUrl = 'http://localhost:5678/api/works'
 
 const fetchPortfolio = async () => {
   try {
@@ -18,5 +16,26 @@ const fetchPortfolio = async () => {
     handleError(error)
   }
 }
-
 export default fetchPortfolio
+// const portfolioApiUrl = 'http://localhost:5678/api/works'
+// 
+// export const handleError = error => {
+//   console.error(error)
+// }
+// 
+// const fetchPortfolio = async () => {
+//   try {
+//     const response = await fetch(portfolioApiUrl, {
+//       method: 'GET',
+//       mode: 'cors'
+//     })
+//     if (!response.ok) {
+//       throw new Error(`Error: ${response.status}`)
+//     }
+//     return response.json()
+//   } catch (error) {
+//     handleError(error)
+//   }
+// }
+// 
+// export default fetchPortfolio
