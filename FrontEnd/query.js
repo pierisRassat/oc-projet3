45e02376a690db7003bb5dc66160837e74818a34
framend <pieris@tuta.io>
2023-02-07 16:57:10 +0100

    remove some useless comments

--- FrontEnd/query.js
+++ FrontEnd/query.js
@@ -1,6 +1,6 @@
 import { handleError } from './error-handler.js'
 
-const portfolioApiUrl = 'http://localhost:5679/ap/works'
+const portfolioApiUrl = 'http://localhost:5678/api/works'
 
 const fetchPortfolio = async () => {
   try {
@@ -17,25 +17,3 @@ const fetchPortfolio = async () => {
   }
 }
 export default fetchPortfolio
-// const portfolioApiUrl = 'http://localhost:5678/api/works'
-// 
-// export const handleError = error => {
-//   console.error(error)
-// }
-// 
-// const fetchPortfolio = async () => {
-//   try {
-//     const response = await fetch(portfolioApiUrl, {
-//       method: 'GET',
-//       mode: 'cors'
-//     })
-//     if (!response.ok) {
-//       throw new Error(`Error: ${response.status}`)
-//     }
-//     return response.json()
-//   } catch (error) {
-//     handleError(error)
-//   }
-// }
-// 
-// export default fetchPortfolio
